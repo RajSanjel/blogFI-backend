89d65f5379291a375071f12d20eeb1083bde440b
Raj Sanjel <rajsanjel201@gmail.com>
2024-06-23 11:45:46 +0545

    fix: 404 error

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 const PORT = process.env.PORT || 3000
 // import path from "path";
+import default from '../frontend/postcss.config';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -37,3 +38,5 @@ app.use("/api", apiRoutes);
 app.listen(PORT, () => {
     console.log("Server is ready at port: " + PORT)
 })
+
+export default app;
